b6e0b786373ca966656d920edb0d73b11b388d78
bence-humi <bence.kiss@humi.ca>
2023-02-21 15:01:18 -0500

    PDS-1395 updated exception to account for json api specs for errors

--- src/Exceptions/JsonApiException.php
+++ src/Exceptions/JsonApiException.php
@@ -3,11 +3,46 @@
 namespace Humi\JsonApiConnector\Exceptions;
 
 use Exception;
+use Throwable;
 
 class JsonApiException extends Exception
 {
+    protected string $errorTitle;
+    protected string $internalErrorCode;
+    protected array $meta;
+
+    public function __construct(
+        $message,
+        $code,
+        $errorTitle = null,
+        $internalCode = null,
+        $meta = [],
+        Throwable $throwable = null
+    )
+    {
+        parent::__construct($message, $code, $throwable);
+        $this->errorTitle = $errorTitle;
+        $this->internalErrorCode = $internalCode;
+        $this->meta = $meta;
+    }
+
     public function getErrors(): array
     {
         return json_decode($this->getMessage(), true)['errors'];
+    }
+
+    public function getErrorTitle()
+    {
+        return $this->errorTitle;
+    }
+
+    public function getInternalErrorCode()
+    {
+        return $this->internalErrorCode;
+    }
+
+    public function getMeta()
+    {
+        return $this->meta;
     }
 }
